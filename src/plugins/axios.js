501c07ee696ae5735bf3ce34ededb9c9e2240d1f
crashmoz <ismail.setiawan81@gmail.com>
2023-07-10 02:56:44 +0700

    change url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost/apishop/'
+axios.defaults.baseURL = 'https://unoverthrown-catche.000webhostapp.com/apishop/'
 
 // const instance = axios.create({
 //   baseURL: 'http://localhost/aapishop/'
